553d371fbde1c98d78d5c799baea4c07184c7ba3
Kochkin n.o 1 -sony music picture internatinol- <ivanovkochkin@iMac-Administrator.local>
2024-03-20 13:51:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/thriller_micahel_jackson_los_angeales_moscow_yakutsk/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.thriller_micahel_jackson_los_angeales_moscow_yakutsk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    TextView view_city;
+    TextView view_temp;
+    TextView view_desc;
+
+    ImageView view_weather;
+    EditText search;
+    FloatingActionButton search_floating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        view_city = findViewById(R.id.town);
+        view_temp = findViewById(R.id.temp);
+        view_desc = findViewById(R.id.desc);
+        view_city.setText("");
+        view_temp.setText("");
+        view_desc.setText("");
+
+        view_weather = findViewById(R.id.weatherimage);
+        search = findViewById(R.id.editTextText);
+        search_floating = findViewById(R.id.floating_search);
+
+        search_floating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getRootView().getWindowToken(), 0 );
+                api_key(String.valueOf(search.getText()));
+            }
+        });
+    }
+
+    private void api_key(String City) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q="+City+"&units=metric&appid=1b47ad3333bd70d8cda1d025e53a2c33")
+                .get()
+                .build();
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                try {
+                    Response response = client.newCall(request).execute();
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                }
+
+
+    private void setImage(final ImageView imageView, final String value){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                switch (value) {
+                    case "01d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon1));
+                        break;
+                    case "01n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon1));
+                        break;
+                    case "02d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon2));
+                        break;
+                    case "02n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon2));
+                        break;
+                    case "03d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon3));
+                        break;
+                    case "03n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon3));
+                        break;
+                    case "04d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon4));
+                        break;
+                    case "04n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon4));
+                        break;
+                    case "09d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon5));
+                        break;
+                    case "09n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon5));
+                        break;
+                    case "10d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon6));
+                        break;
+                    case "10n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon6));
+                        break;
+                    case "11d": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon7));
+                        break;
+                    case "11n": imageView.setImageDrawable(getResources().getDrawable(R.drawable.icon7));
+                        break;
+
+                    default:
+                        imageView.setImageDrawable(getResources().getDrawable(R.drawable.weather));
+                }
+            }
+        });
+    }
+}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "thriller_micahel_jackson_los_angeales_moscow_yakutsk"
+include(":app")
+ 
